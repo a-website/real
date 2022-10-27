2765936aa3746a9cf6d0ba8e64ecbf200ff40e05
a-website <45741200+a-website@users.noreply.github.com>
2022-10-27 19:53:04 -0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,15 @@
+local coregui = game.CoreGui:GetDescendants()
+while true do
+    task.wait()
+    for _, object in ipairs(coregui) do
+        if object:IsA("ImageLabel") or object:IsA("ImageButton") then
+            object.ImageTransparency = 1
+        end
+        if object:IsA("TextLabel") then
+            object.TextTransparency = 1
+        end
+        if object:IsA("Frame") then
+            object.BackgroundTransparency = 1
+        end
+    end
+end
