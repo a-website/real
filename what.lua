9a79a75e82ae8fd8faccf0bbe48e4484aa3456ca
a-website <45741200+a-website@users.noreply.github.com>
2022-10-27 20:17:20 -0400

    Create what.lua

---
+++ what.lua
@@ -0,0 +1,20 @@
+repeat wait() until game:IsLoaded()
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Health, false)
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
+local coregui = game.CoreGui:GetDescendants()
+while true do
+    task.wait()
+    for _, object in ipairs(coregui) do
+        if object:IsA("ImageLabel") or object:IsA("ImageButton") then
+            object.ImageTransparency = 1
+        end
+        if object:IsA("TextLabel") then
+            object.TextTransparency = 1
+        end
+        if object:IsA("Frame") then
+            object.BackgroundTransparency = 1
+        end
+    end
